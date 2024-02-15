332e8d906f9e2288a3afcd1293f9a8cc43d3813b
kimjiyun <faren02134@gmail.com>
2024-02-15 20:15:04 +0900

    샤리

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import BoilingVerdict from './BoilingVerdict';
+import Calculater from './Calculater';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Calculater />
+    </div>
+  );
+}
+
+export default App;
